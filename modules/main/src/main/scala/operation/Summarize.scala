1084053b9188eb8c96463d91a282b89ce1d08e1b
Rob Norris <rob_norris@mac.com>
2020-05-01 13:32:47 -0500

    sort order for summarize

--- modules/main/src/main/scala/operation/Summarize.scala
+++ modules/main/src/main/scala/operation/Summarize.scala
@@ -14,24 +14,69 @@ import cats.effect.Blocker
 import itac.ObservationDigest
 import gsp.math.Angle
 import gsp.math.HourAngle
+import cats.Order
 import edu.gemini.tac.qengine.p1.Observation
+import cats.data.NonEmptyList
 
 object Summarize {
 
-  def apply[F[_]: Sync](reference: String): Operation[F] =
+  private implicit class ObservationOps(o: Observation) {
+    def ra:  HourAngle = Angle.hourAngle.get(Angle.fromDoubleDegrees(o.target.ra.mag))
+    def dec: Angle     = Angle.fromDoubleDegrees(o.target.dec.mag)
+  }
+
+  final case class BandedObservation(band: String, obs: Observation)
+
+  final case class Field(name: String, order: Order[BandedObservation])
+  object Field {
+
+    val band = Field("band",  Order.by(o => o.band))
+    val hash = Field("hash",  Order.by(o => ObservationDigest.digest(o.obs.p1Observation)))
+    val ra   = Field("ra",    Order.by(o => o.obs.ra.toDoubleDegrees))
+    val dec  = Field("dec",   Order.by(o => o.obs.dec.toSignedDoubleDegrees))
+    val time = Field("time",  Order.by(o => o.obs.time.toHours.value))
+    val name = Field("name",  Order.by(o => o.obs.target.name.orEmpty.toLowerCase()))
+
+    val all: List[Field] =
+      List(band, hash, ra, dec, time, name)
+
+    def fromString(name: String): Either[String, Field] =
+      all.find(_.name.toLowerCase == name)
+         .toRight(s"No such field: $name. Try one or more of ${all.map(_.name).mkString(",")}")
+
+    def parse(ss: String): Either[String, NonEmptyList[Field]] =
+      ss.split(",")
+        .toList
+        .traverse(fromString)
+        .flatMap { fs =>
+          NonEmptyList
+            .fromList(fs)
+            .toRight(s"No fields specified. Try one or more of ${all.map(_.name).mkString(",")}")
+        }
+
+  }
+
+
+  def apply[F[_]: Sync](reference: String, fields: NonEmptyList[Field]): Operation[F] =
     new Operation[F] {
 
       def summarize(p: Proposal): F[Unit] =
         Sync[F].delay {
 
+          val order = fields.reduceMap(_.order)(Order.whenEqualMonoid)
+
           def printObs(band: String)(o: Observation): Unit = {
             val id     = ObservationDigest.digest(o.p1Observation)
-            val ra     = HourAngle.HMS(Angle.hourAngle.get(Angle.fromDoubleDegrees(o.target.ra.mag))).format
-            val dec    = Angle.DMS(Angle.fromDoubleDegrees(o.target.dec.mag)).format
+            val ra     = HourAngle.HMS(o.ra).format
+            val dec    = Angle.DMS(o.dec).format
             val coords = f"${o.conditions.cc}%-5s ${o.conditions.iq}%-5s ${o.conditions.sb}%-5s ${o.conditions.wv}%-5s "
             val hrs    = o.time.toHours.value
             println(f"$id $band%-4s $hrs%5.1fh $coords $ra%16s $dec%16s ${o.target.name.orEmpty}")
           }
+
+          val obsList: List[BandedObservation] =
+            p.obsList.map(BandedObservation("B1/2", _)) ++
+            p.band3Observations.map(BandedObservation("B3", _))
 
           println()
           println(s"Reference: ${p.id.reference}")
@@ -40,8 +85,15 @@ object Summarize {
           println(s"Partner:   ${p.ntac.partner.fullName}")
           println(f"Award:     ${p.time.toHours.value}%1.1f hours")
           println()
-          p.obsList.foreach(printObs("B1/2"))
-          p.band3Observations.foreach(printObs("B3"))
+
+          obsList
+            // remove duplicates ... ?
+            // .groupBy(o => ObservationDigest.digest(o.obs.p1Observation))
+            // .values.map(_.head)
+            // .toList
+            .sorted(order.toOrdering)
+            .foreach(bo => printObs(bo.band)(bo.obs))
+
           println()
         }
 
