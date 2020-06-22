faae3dcd8bfe4b8e4533cf1e15899760fee36f77
Rob Norris <rob_norris@mac.com>
2020-06-22 16:11:41 -0500

    add split times and itac comment

---
+++ modules/main/src/main/scala/operation/ProgramPartnerTime.scala
@@ -0,0 +1,41 @@
+// Copyright (c) 2016-2019 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package operation
+
+import cats._
+import cats.implicits._
+import edu.gemini.tac.qengine.p1.QueueBand
+import edu.gemini.tac.qengine.p1.Proposal
+import edu.gemini.model.p1.immutable.TimeAmount
+import edu.gemini.tac.qengine.util.Time
+
+object ProgramPartnerTime {
+
+  private implicit val m: Monoid[TimeAmount] =
+    new Monoid[TimeAmount] {
+      def combine(x: TimeAmount, y: TimeAmount): TimeAmount = x |+| y
+      def empty: TimeAmount = TimeAmount.empty
+    }
+
+  def programAndPartnerTime(p: Proposal, band: QueueBand): (Time, Time) = {
+
+    // Original estimated times from the proposal
+    val (progTime, partTime): (TimeAmount, TimeAmount) =
+      p.obsListFor(band)
+       .map(_.p1Observation)
+       .foldMap(o => (o.progTime.orEmpty, o.partTime.orEmpty))
+
+    // Factor with respect to the awarded time
+    val ratio: Double = progTime.toHours.value / (progTime |+| partTime).toHours.value
+
+    // Scale the prog and program time
+    val progTimeʹ = Time.millisecs((p.time.ms * ratio).toLong).toHours
+    val partTimeʹ = Time.millisecs((p.time.ms * (1.0 - ratio)).toLong).toHours
+
+    // Done
+    (progTimeʹ, partTimeʹ)
+
+  }
+
+}
