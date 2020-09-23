855f0d57fed6dcb17144ab1752f4451828afba42
Rob Norris <rob_norris@mac.com>
2020-09-23 15:02:33 -0500

    remove script

--- sanity-check.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-echo
-echo "👉 diff with gs baseline"
-echo
-bloop run main -- --dir work queue -s 2> /dev/null | diff baseline-gs -
-echo
-echo "👉 diff with gs baseline"
-echo
-bloop run main -- --dir work queue -n 2> /dev/null | diff baseline-gn -
