e7b20216f09312981c9381c5a042943fda0eb4e0
Rob Norris <rob_norris@mac.com>
2020-09-25 12:18:24 -0500

    fix lub

--- modules/engine/src/main/scala/p1/CategorizedTime.scala
+++ modules/engine/src/main/scala/p1/CategorizedTime.scala
@@ -5,7 +5,7 @@ import edu.gemini.tac.qengine.util.Time
 /**
  * An amount of time categorized by target and conditions.
  */
-trait CategorizedTime {
+trait CategorizedTime extends Product with Serializable {
   def target: Target
   def conditions: ObservingConditions
   def time: Time
