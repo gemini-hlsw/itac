4ae152c70d61481a5e5ff3c62902b234848e673a
Rob Norris <rob_norris@mac.com>
2021-09-17 10:05:08 -0500

    header

--- modules/main/src/main/scala/ReferenceTime.scala
+++ modules/main/src/main/scala/ReferenceTime.scala
@@ -1,12 +1,14 @@
+// Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
 package itac
-import itac.ReferenceTime.Default
-import itac.ReferenceTime.At
 
 /**
  * A reference time to use when determining target coordinates. Nonsidereal target edits can specify
  * a non-default reference time via an edit.
  */
 sealed trait ReferenceTime {
+  import ReferenceTime._
   def getOrElse(default: Long): Long =
     this match {
       case Default  => default
